09f4b30fb1f4a95d69718450419c0b79ed311682
Daniel Batubara <dn.batubara@gmail.com>
2017-05-22 09:58:44 +0200

    Update my-name-is-daniel.js

--- my-name-is-daniel.js
+++ my-name-is-daniel.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
+// 	google analytics function
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	$(window).resize(function(){location.reload();});
-
+	  ga('create', 'UA-99701287-1', 'auto');
+	  ga('send', 'pageview');
+	
+	
+// 	location.reload();
 	var educationDiv = $("#education-section").offset().top;
 	var experienceDiv = $("#experience-section").offset().top;
 	var skillsDiv = $("#skills-section").offset().top;
 	var projectDiv = $("#project-section").offset().top;
+	$(window).resize(function(){
+		educationDiv = $("#education-section").offset().top;
+		experienceDiv = $("#experience-section").offset().top;
+		skillsDiv = $("#skills-section").offset().top;
+		projectDiv = $("#project-section").offset().top;
+	});
 	$(document).scroll(function() {
     if($(window).scrollTop()>educationDiv) {
   	$('#education-section').removeClass("background2");
